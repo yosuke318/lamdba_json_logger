140d3256e2b50d616005bc88fc74442d2ea26d33
Yosuke Maruyama <yosuke.maruyama@whitecanvas.jp>
2024-12-21 13:21:27 +0900

    setup lambda_json_logger.py

---
+++ lambda_json_logger/lambda_json_logger.py
@@ -0,0 +1,37 @@
+from logging import getLogger, StreamHandler, DEBUG, CRITICAL, ERROR, WARNING, INFO
+import logging ,json
+
+LOG_LEVELS = {
+    "CRITICAL" : CRITICAL,
+    "ERROR"    : ERROR,
+    "WARNING"  : WARNING,
+    "INFO"     : INFO,
+    "DEBUG"    : DEBUG,
+}
+
+
+def getLambdaJsonLoggerInstance(level=DEBUG, aws_request_id=None, stage=None):
+    # Getting logger instance
+    logger = getLogger("lambda_json_logger")
+    logger.setLevel(level)
+
+    if not logger.handlers:
+        stream_handler = StreamHandler()
+
+        class LambdaJsonFormatter(logging.Formatter):
+            def format(self, record):
+                record = {
+                    "time": self.formatTime(record),
+                    "level": record.levelname,
+                    "message": record.getMessage(),
+                    "function_name": record.funcName,
+                    "module": record.module,
+                    "aws_request_id": record.aws_request_id if hasattr(record, 'aws_request_id') else None,
+                    "stage": record.stage if hasattr(record, 'stage') else "unknown",
+                }
+                return super().format(json.dumps(record))
+
+        stream_handler.setFormatter(LambdaJsonFormatter())
+        logger.addHandler(stream_handler)
+
+    return logger
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name="lambda_json_logger",
+    version="1.0",
+    python_requires='>=3.11',
+    packages=find_packages(
+
+    )
+)

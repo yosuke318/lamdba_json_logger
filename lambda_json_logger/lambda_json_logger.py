aeeb829edc9b660d0c6e67dc8d7eba1c6b33188c
Marco <youman.318@gmail.com>
2025-01-09 10:13:47 +0900

    setup lambda_json_logger.py

--- lambda_json_logger/lambda_json_logger.py
+++ lambda_json_logger/lambda_json_logger.py
@@ -29,7 +29,7 @@ def getLambdaJsonLoggerInstance(level=DEBUG, aws_request_id=None, stage=None):
                     "aws_request_id": aws_request_id if hasattr(record, 'aws_request_id') else None,
                     "stage": stage if hasattr(record, 'stage') else "unknown",
                 }
-                return super().format(record)
+                return json.dumps(record)
 
         stream_handler.setFormatter(LambdaJsonFormatter())
         logger.addHandler(stream_handler)
